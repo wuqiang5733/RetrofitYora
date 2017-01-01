5ba74f315a5c3718d3978c29501f043db7749f34
xiaoqianghou <2662651103@qq.com>
2017-01-01 19:18:17 +0800

    异步

--- app/src/main/java/com/xuxiaoxiao/retrofityora/MainActivity.java
+++ app/src/main/java/com/xuxiaoxiao/retrofityora/MainActivity.java
@@ -3,15 +3,22 @@ package com.xuxiaoxiao.retrofityora;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import retrofit.Callback;
 import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 import retrofit.http.GET;
+import retrofit.http.Path;
 import retrofit.http.Query;
 
 public class MainActivity extends AppCompatActivity {
@@ -50,12 +57,30 @@ public class MainActivity extends AppCompatActivity {
         public List<UserSummery> items;
     }
 
+    private class UserDetails{
+        public String id;
+        public String location;
+
+        @Override
+        public String toString(){
+            return "UserDetails { id = " + id + "location = " + location;
+        }
+    }
+
     private interface GithubService{
         @GET("/gists/public")
         List<Gist> getPublicGists();
 
         @GET("/search/users")
         UsersSearchResult searchUsers(@Query("q") String query);
+
+        @GET("users/{username}")
+        UserDetails getUser(@Path("username") String username);
+
+        // 下面是演示在另一个线程执行的情况
+        @GET("users/{username}")
+        void getUserAsync(@Path("username") String username , Callback<UserDetails> callback);
+
 
     }
 
@@ -70,14 +95,31 @@ public class MainActivity extends AppCompatActivity {
                 .setEndpoint("https://api.github.com")
                 .build();
 
-        GithubService service = adapter.create(GithubService.class);
+        final GithubService service = adapter.create(GithubService.class);
 
-        ArrayAdapter<Object> listAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1);
+        final ArrayAdapter<Object> listAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1);
         ListView listView = (ListView) findViewById(R.id.activity_main_listView);
         listView.setAdapter(listAdapter);
 
         listAdapter.addAll(service.searchUsers("wuqiang5733").items);
 //        listAdapter.addAll(service.searchUsers("wuqiang5733"));
 //        listAdapter.addAll(service.getPublicGists());
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                UserSummery summery = (UserSummery)listAdapter.getItem(position);
+                service.getUserAsync(summery.login, new Callback<UserDetails>() {
+                    @Override
+                    public void success(UserDetails userDetails, Response response) {
+                        Toast.makeText(MainActivity.this,userDetails.location,Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void failure(RetrofitError error) {
+
+                    }
+                });
+            }
+        });
     }
 }
